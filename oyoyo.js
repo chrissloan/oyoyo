9c8d78212193d27b6034b47be319af422d2f28dd
chrissloan <chris@chrissloan.info>
2008-10-28 10:08:57 -0400

    Previous and next functionality added

--- oyoyo.js
+++ oyoyo.js
@@ -15,10 +15,13 @@ var yoSlider = Class.create({
 	
 	options: {
 		sliderHolderClass: "slider_holder",
-		sliderElementsContainerClass: "slider_innards",
+		sliderTrackClass: "slider_track",
 		sliderElementClass: "slider_item",
 		sliderNextButton: "slider_next",
-		sliderPreviousButton: "slider_previous"
+		sliderPreviousButton: "slider_previous",
+		slideBy: 1,
+		useTabs: false,
+		sliderTabsClass: "slider_tabs"
 	},
 	
 	initialize: function(element, options){
@@ -28,21 +31,68 @@ var yoSlider = Class.create({
 		this.el = $('element');
 		this.elid = element;
 		
-		this.sliderHolders = $$('#' + this.elid + ' .' + this.options.sliderHolderClass);
-		this.sliderElementsContainer = $$('#' + this.elid + ' .' + this.options.sliderElementsContainerClass);
-		this.sliderElements = $$('#' + this.elid + ' .' + this.options.sliderHolderClass);
+		this.sliderHolder = $$('#' + this.elid + ' .' + this.options.sliderHolderClass);
+		this.sliderTrack = $$('#' + this.elid + ' .' + this.options.sliderTrackClass);
+		this.sliderElements = $$('#' + this.elid + ' .' + this.options.sliderElementClass);
+		
+		if(this.options.useTabs){
+			this.tabsObject = $$('.' + this.options.sliderTabsClass);
+			console.log(this.tabsObject);
+			//this.tabs = this.tabsObject.childElements();
+		}
+		
 		this.nextButtons = $$('#' + this.elid + ' .' + this.options.sliderNextButton);
 		this.previousButtons = $$('#' + this.elid + ' .' + this.options.sliderPreviousButton);
 		
+		this.elWidth = this.sliderElements[0].getWidth() * this.options.slideBy;
+		
+		this.trackWidth = this.elWidth * this.sliderElements.length;
+		this.totalLength = ((this.sliderElements.length - 1) * (this.elWidth))  - (this.elWidth * this.options.slideBy);
+		console.log(this.totalLength);
+		this.elHeight = 0;
+		this.sliderElements.each(function(item){
+			if(item.getHeight() > this.elHeight){
+				this.elHeight = item.getHeight();
+			}
+		}.bind(this));
+		
+		this.sliderTrack[0].setStyle({'width': this.trackWidth + "px"});
+		this.sliderHolder[0].setStyle({'height': this.elHeight + "px", 'width': this.elWidth + "px"});
+
+		
 		this.nextButtons.each(function(item, index){
 			item.observe('click', function(item, index){
+				
 				this.slideNext(index);
+				//this.checkPosition();
+      }.bindAsEventListener(this,index));
+		}.bind(this));
+		
+		this.previousButtons.each(function(item, index){
+			item.observe('click', function(item, index){
+				this.slidePrevious(index);
       }.bindAsEventListener(this,index));
 		}.bind(this));
 	},
 	
 	slideNext: function(index){
-		alert(index);
+		this.currentPosition = this.sliderTrack[0].positionedOffset();
+		
+		if(this.currentPosition.left == -this.totalLength){
+			new Effect.Move(this.sliderTrack[0], {x:this.totalLength});
+		}else{
+			new Effect.Move(this.sliderTrack[0], {x:-this.elWidth});
+		}
+
+	},
+	
+	slidePrevious: function(index){
+		this.currentPosition = this.sliderTrack[0].positionedOffset();
+		if(this.currentPosition.left == 0){
+			new Effect.Move(this.sliderTrack[0], {x:-this.totalLength});
+		}else{
+			new Effect.Move(this.sliderTrack[0], {x:this.elWidth});
+		}
 	}
 	
 
